2c0b2972061a626382655dd4ed78c0546847e9a0
Luis Pedro Coelho <luis@luispedro.org>
2021-01-13 13:14:07 +0800

    RFCT Break out population into a class

--- simulate.py
+++ simulate.py
@@ -1,34 +1,60 @@
 import numpy as np
 N = 450_000_000
 VD = 1_000_000
+N2 = 20
 
 class StrictlySecond:
-    def v2(self, vd, state):
-        available = state[-2]
+    def v2(self, vd, pop):
+        available = pop.ready_for_2()
         return min(available, vd)
 
 
 class StrictlyFirst:
-    def v2(self, vd, state):
-        missing = N - state.sum()
+    def v2(self, vd, pop):
+        missing = pop.ready_for_1()
         return max(0, vd - missing)
 
+class Population:
+    def __init__(self, total):
+        self.total = total
+        self.vaccinated1 = np.array([])
+        self.vaccinated2 = 0
+
+    def next_day(self):
+        self.vaccinated1 = np.concatenate([[0], self.vaccinated1])
+
+    def vaccinate(self, v1, v2):
+        self.vaccinated1[0] = v1
+        while v2 > 0:
+            v = max(v2, self.vaccinated1[-1])
+            v2 -= v
+            self.vaccinated1[-1] -= v
+            self.vaccinated2 += v
+            if self.vaccinated1[-1] == 0:
+                self.vaccinated1 = self.vaccinated1[:-1]
+
+    def ready_for_1(self):
+        return self.total - self.vaccinated2 - self.vaccinated1.sum()
+
+    def ready_for_2(self):
+        return self.vaccinated1[N2:].sum()
+    
+    def fully_protected(self):
+        return self.vaccinated2 == self.total
+
 def simulate(strategy, p1, p2):
+    pop = Population(N)
     P = []
-    Vstate = np.zeros(shape=22)
-    while Vstate[-1] < N:
-        Vstate = np.concatenate([[0], Vstate[:-3], [Vstate[-2]+Vstate[-3], Vstate[-1]]])
-        ready_for_2 = Vstate[-2]
-        protection = p2 * Vstate[-1] + p1 * Vstate[12:-1].sum()
+    while not pop.fully_protected():
+        pop.next_day()
+        ready_for_2 = pop.ready_for_2()
+        protection = p2 * pop.vaccinated2 + p1 * pop.vaccinated1[11:].sum()
         P.append(protection)
-        v2 = strategy.v2(VD, Vstate)
-        Vstate[-1] += v2
-        Vstate[-2] -= v2
-        Vstate[0] = VD - v2
+        v2 = strategy.v2(VD, pop)
+        pop.vaccinate(VD - v2, v2)
     return np.array(P)
 
 P1 = simulate(StrictlyFirst(), p1=.1, p2=.95)
 P2 = simulate(StrictlySecond(), p1=.1, p2=.95)
 print(P1.sum()/P2.sum())
 
-(P1[:900]+1)/(1+P2[:900])
